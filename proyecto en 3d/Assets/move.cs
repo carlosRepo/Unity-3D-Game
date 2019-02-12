33c5c39c7c79f9951c9241c885fa48d1a8379e92
carlospoqw <carlospoqw@gmail.com>
2019-02-12 15:26:39 -0300

    primer commit
    movimiento jugador, camara y salto limitado

---
+++ proyecto en 3d/Assets/move.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float rotX;
+    public float rotY;
+    public float rotZ;
+    public int saltos;
+    public Rigidbody rb;
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>();
+        saltos=20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moverse
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal"),0f,moveSpeed*Input.GetAxis("Vertical"));
+        rotX -= Input.GetAxis("Mouse Y")*Time.deltaTime*rotationSpeed;
+        rotY += Input.GetAxis("Mouse X")*Time.deltaTime*rotationSpeed;
+        //tener un limite al mirar arriba o abajo
+        if (rotX< -90)
+        {
+            rotX=-90;
+        }else if (rotX>90){
+            rotX=90;
+        }
+        //que rote segun se mueva el mouse
+        transform.rotation= Quaternion.Euler(0,rotY,0);
+        //mover camara donde el mouse
+        GameObject.FindGameObjectsWithTag("MainCamera")[0].transform.rotation=Quaternion.Euler(rotX,rotY,0);
+        //saltar 
+        if (Input.GetButtonDown("Jump")&&saltos>0)
+        {
+            rb.velocity = new Vector3(0,5,0);
+            saltos-=1;
+        }
+    }
+
+    void FixedUpdate(){
+
+    }
+}
---
+++ proyecto en 3d/Assets/playerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform camTransform;
+    public Camera cam;
+
+    public float distance =10.0f;
+    public float currentY =0.0f;
+    public float currentX =0.0f;
+    public float sensivityX =4.0f;
+    public float sensivityY =1.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        camTransform = transform;
+        cam=Camera.main;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 dir= new Vector3(0,0,-distance);
+        Quaternion rotation = Quaternion.Euler(currentX,currentY,0);
+        camTransform.position= lookAt.position + rotation * dir;
+    }
+}
